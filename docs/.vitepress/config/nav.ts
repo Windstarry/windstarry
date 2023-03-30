df6635b471a6c80c021222a034de9812a1ca2b80
windstarry <liefengwind@163.com>
2023-03-30 15:45:07 +0800

    增加首页导航

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -1,14 +1,15 @@
 export default [
-        {
-          text: '学习',
-          items: [
-            { text: 'python', link: '/learn/python/' },
-            { text: 'vue3', link: '/learn/vue3/' },
-            { text: '博客建站', link: '/learn/webblog/' },
-          ]
-        },
-        {
-          text: '关于我',
-          link: '/about-me'
-        },
-      ];
+  { text: "首页", link: "/" },
+  {
+    text: "学习",
+    items: [
+      { text: "python", link: "/learn/python/" },
+      { text: "vue3", link: "/learn/vue3/" },
+      { text: "博客建站", link: "/learn/webblog/" },
+    ],
+  },
+  {
+    text: "关于我",
+    link: "/about-me",
+  },
+];
